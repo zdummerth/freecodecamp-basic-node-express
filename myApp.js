44b01fbba35e1926185aeab5c8be7587c3080472
Zach Dummerth <zdummerth@gmail.com>
2023-08-25 18:56:50 -0500

    add index page to root route

--- myApp.js
+++ myApp.js
@@ -1,10 +1,8 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
-
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
