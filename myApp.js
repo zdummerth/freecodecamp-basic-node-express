8c33b29af99ec4b0166916f7095a8e1e5cd4e12e
Zach Dummerth <zdummerth@gmail.com>
2023-08-25 19:08:30 -0500

    add json route

--- myApp.js
+++ myApp.js
@@ -5,6 +5,12 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  let message = "Hello json";
+
+  res.json({ message });
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
