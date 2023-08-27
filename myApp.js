2d5d4bd2afbeefba6fd1f4b87ef1ef1485b0d5fc
Zach Dummerth <zdummerth@gmail.com>
2023-08-27 12:40:11 -0500

    add now route

--- myApp.js
+++ myApp.js
@@ -7,6 +7,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
@@ -20,6 +22,15 @@ app.get("/json", (req, res) => {
   res.json({ message });
 });
 
-app.use("/public", express.static(__dirname + "/public"));
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
 module.exports = app;
